2d39aa669b6225c3548c373c33d68e4a5843b915
saipranayteja <pranayteja.ch@algonox.com>
2023-04-15 10:19:41 +0530

    go

--- tags.tf
+++ tags.tf
@@ -1,6 +1,6 @@
 resource "aws_ec2_tag" "spot_instance_tags" {
     for_each = var.ansible-practise
-    resource_id = aws_spot_instance_request.elk.spot_instance_id
+    resource_id = aws_spot_instance_request.elk[each.key].spot_instance_id
     key         = "Name"
     value       = each.value["name"]
     #value       = each.key
