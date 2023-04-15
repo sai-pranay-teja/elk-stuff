0dfd4d1dd4d6a36ac9ae2ac2c099745d34a657cd
saipranayteja <pranayteja.ch@algonox.com>
2023-04-15 10:15:08 +0530

    go

---
+++ tags.tf
@@ -0,0 +1,7 @@
+resource "aws_ec2_tag" "spot_instance_tags" {
+    for_each = var.ansible-practise
+    resource_id = aws_spot_instance_request.elk.spot_instance_id
+    key         = "Name"
+    value       = each.value["name"]
+    #value       = each.key
+}
